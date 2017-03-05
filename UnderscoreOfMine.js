1f095a35cd9cbe8d425c5994e38b86bb3c8d522a
Nzybwzy <781746210@qq.com>
2017-03-05 23:10:30 +0800

    添加方法

--- UnderscoreOfMine.js
+++ UnderscoreOfMine.js
@@ -1,5 +1,10 @@
 (function() {
+  // 将 this 赋值给局部变量 root
+  // root 的值, 客户端为 "window"
 	var root = this;
+  // 核心函数 "_" 其实是一个构造函数 无 new 调用的构造函数
+  // 将传入的参数（实际要操作的数据）赋值给 this._wrapped 属性
+  // OOP 调用时，_ 相当于一个构造函数
 	var _ = function(obj) {
     if (obj instanceof _)
       return obj;
@@ -16,6 +21,11 @@
   } else {
     root._ = _;
   }
+
+  // Math.pow(2, 53) - 1 是 JavaScript 中能精确表示的最大数字
+  var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
+
+  // 返回一个 [min, max] 范围内的任意整数
   _.random = function(min, max) {
     if (max == null) {
       max = min;
@@ -23,4 +33,31 @@
     }
     return min + Math.floor(Math.random() * (max - min + 1));
   };
+  // Underscore并没有在原生的JavaScript对象原型中进行扩展，而是像jQuery一样，将数据封装在一个自定义对象中（下文中称“Underscore对象”）。
+  // 你可以通过调用一个Underscore对象的value()方法来获取原生的JavaScript数据
+  _.prototype.value = function() {
+    return this._wrapped;
+  };
+
+  var property = function(key) {
+    return function(obj){
+      return obj == null ? void 0 : obj[key]
+    }
+  }
+  // 该函数传入一个参数，返回参数的 length 属性值
+  // 用来获取 array(数组) 以及 arrayLike(类数组) 元素的 length 属性值
+  var getLength = property('length');
+  _.getLength = getLength;
+
+  // 判断是否是 ArrayLike Object
+  // 数组 类数组，即拥有 length 属性并且 length 属性值为 Number 类型的元素
+  // 包括数组、arguments、HTML Collection 以及 NodeList 等等 包括字符串、函数等
+  var isArrayLike = function(collection) {
+    // 返回参数 collection 的 length 属性值
+    var length = getLength(collection);
+    return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
+  };
+  _.isArrayLike = isArrayLike;
+  // 返回一个函数，这个函数返回任何传入的对象的key属性值。
+  _.property = property;
 }.call(this))
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log(_.random(1, 2) )
+console.log(_.random(1, 10));
+
+// 定义一个JavaScript内置对象  
+var jsData = {  
+    name : 'data'  
+};
+
+// 通过_()方法将对象创建为一个Underscore对象  
+// underscoreData对象的原型中包含了Underscore中定义的所有方法，你可以任意使用  
+var underscoreData = _(jsData);
+
+// 通过value方法获取原生数据, 即jsData  
+underscoreData.value();
+
+var stooge = {name: 'moe', age: 16};
+console.log(_.property('age')(stooge));
+function fnlen(q,w,e,r) {}
+console.log(_.getLength(fnlen))
+console.log(_.isArrayLike(fnlen))
+
+
