43dc8eaa40a9d17748d455057366ead070a55583
Nzybwzy <781746210@qq.com>
2017-03-09 22:43:15 +0800

    添加缓存变量并且优先使用es5原生方法

--- UnderscoreOfMine.js
+++ UnderscoreOfMine.js
@@ -1,7 +1,9 @@
 (function() {
+
   // 将 this 赋值给局部变量 root
   // root 的值, 客户端为 "window"
 	var root = this;
+
   // 核心函数 "_" 其实是一个构造函数 无 new 调用的构造函数
   // 将传入的参数（实际要操作的数据）赋值给 this._wrapped 属性
   // OOP 调用时，_ 相当于一个构造函数
@@ -12,7 +14,32 @@
       return new _(obj);
     this._wrapped = obj;
   };
-  
+
+  // 将原来全局环境中的变量 `_` 赋值给变量 previousUnderscore 进行缓存
+  // 在后面的 noConflict 方法中有用到
+  var previousUnderscore = root._;
+
+  // 缓存变量, 便于压缩代码
+  var ArrayProto = Array.prototype, ObjProto = Object.prototype, FuncProto = Function.prototype;
+
+  // 缓存变量, 便于压缩代码
+  // 同时可减少在原型链中的查找次数(提高代码效率)
+  var
+    push             = ArrayProto.push,
+    slice            = ArrayProto.slice,
+    toString         = ObjProto.toString,
+    hasOwnProperty   = ObjProto.hasOwnProperty;
+
+  // ES5 原生方法, 如果浏览器支持, 则 underscore 中会优先使用
+  var
+    nativeIsArray      = Array.isArray,
+    nativeKeys         = Object.keys,
+    nativeBind         = FuncProto.bind,
+    nativeCreate       = Object.create;
+
+  // 将上面定义的 `_` 局部变量赋值给全局对象中的 `_` 属性
+  // 即客户端中 window._ = _
+  // 这样暴露给全局后便可以在全局环境中使用 `_` 变量(方法)
   if (typeof exports !== 'undefined') {
     if (typeof module !== 'undefined' && module.exports) {
       exports = module.exports = _;
